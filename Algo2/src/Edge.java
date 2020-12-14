0877c688c722db12e7d62a9be7b69c72d23e685e
armandfargeon <54988442+armandfargeon@users.noreply.github.com>
2020-12-14 22:51:06 +0100

    edge label

--- Algo2/src/Edge.java
+++ Algo2/src/Edge.java
@@ -5,19 +5,21 @@ import java.util.List;
 public class Edge {
     Graph.Color color;
     List<Vertex> vertices = new ArrayList<>();
+    String label;
 
     /* No neighbors */
-    Edge(Graph.Color color){
+    Edge(String label, Graph.Color color){
+        this.label = label;
         this.color = color;
     }
 
-    Edge(Graph.Color color, Edge neighbor1, Graph.Color vertexColor){
-        this(color);
+    Edge(String label, Graph.Color color, Edge neighbor1, Graph.Color vertexColor){
+        this(label, color);
         vertices.add(new Vertex(neighbor1, vertexColor));
     }
 
-    Edge(Graph.Color color, Edge neighbor1, Graph.Color vertexColor, Edge neighbor2, Graph.Color vertexColor2){
-        this(color, neighbor1, vertexColor);
+    Edge(String label, Graph.Color color, Edge neighbor1, Graph.Color vertexColor, Edge neighbor2, Graph.Color vertexColor2){
+        this(label, color, neighbor1, vertexColor);
         vertices.add(new Vertex(neighbor2, vertexColor2));
     }
 }
