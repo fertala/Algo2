8db849b67fd8cde3197b374c82c56b9a129abb6d
armandfargeon <54988442+armandfargeon@users.noreply.github.com>
2020-12-14 22:45:20 +0100

    edges, vertex

--- Algo2/src/Edge.java
+++ Algo2/src/Edge.java
@@ -1,2 +1,23 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Edge {
+    Graph.Color color;
+    List<Vertex> vertices = new ArrayList<>();
+
+    /* No neighbors */
+    Edge(Graph.Color color){
+        this.color = color;
+    }
+
+    Edge(Graph.Color color, Edge neighbor1, Graph.Color vertexColor){
+        this(color);
+        vertices.add(new Vertex(neighbor1, vertexColor));
+    }
+
+    Edge(Graph.Color color, Edge neighbor1, Graph.Color vertexColor, Edge neighbor2, Graph.Color vertexColor2){
+        this(color, neighbor1, vertexColor);
+        vertices.add(new Vertex(neighbor2, vertexColor2));
+    }
 }
